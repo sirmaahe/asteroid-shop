f6600ea2f2536c00f2e1bfdaf0f60ad06afc23e4
turbojesus <sirmaahe@gmail,com>
2017-04-09 16:00:48 +0300

    magic trick3

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
-heroku config:set DISABLE_COLLECTSTATIC=1
 release: python manage.py migrate
 web: gunicorn asteroid_shop.wsgi --log-file -
--- asteroid_shop/settings.py
+++ asteroid_shop/settings.py
@@ -147,11 +147,18 @@ USE_L10N = True
 USE_TZ = True
 
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
